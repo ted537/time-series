355a109f2956672ea42a322aac9e66f13a84ba53
Ted Brownlow <ted.brownlow@gmail.com>
2021-04-17 14:40:59 -0400

    added time scaling

--- app.jsx
+++ app.jsx
@@ -13,19 +13,19 @@ import {
 import { TimeSeries, TimeRange } from "pondjs";
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 1000,
-	// center overall content
-	margin:"0 auto"
-  },
-});
+	root: {
+		// center overall content
+		margin:"0 auto",
+		maxWidth:1000
+	}
+})
 
 const X_STEP = 0.01;
 // since react-timeseries-charts scales up numbers into dates,
 // we should scale up  these values to avoid rounding errors.
 // In future, we should probably use a library 
 // that isn't focused on time-series specifically
-const TIME_SCALE = 100;
+const TIME_SCALE = 1000000;
 function makeSeries(xRange,f) {
 	const [xMin,xMax] = xRange;
 	const points = [];
@@ -39,10 +39,12 @@ function makeSeries(xRange,f) {
 	});
 }
 
+const formatXAxis = x => x.getTime()/TIME_SCALE
+
 function Chart({xRange,f}) {
 	const series = makeSeries(xRange,f);
-	const timeRange = new TimeRange(xRange[0]*100,xRange[1]*100);
-	return <ChartContainer timeRange={timeRange} width={800}>
+	const timeRange = new TimeRange(xRange[0]*TIME_SCALE,xRange[1]*TIME_SCALE);
+	return <ChartContainer timeRange={timeRange} width={800} format={formatXAxis}>
 		<ChartRow height="200">
 			<YAxis 
 				id="y" label="y" 
@@ -59,10 +61,9 @@ function Chart({xRange,f}) {
 }
 
 function ChartView() {
-	const classes = useStyles();
 	const [xRange,setXRange] = useState([0,10]);
 	const [xMin,xMax] = xRange;
-	return <div className={classes.root}>
+	return <div>
 		<Slider
 			value={xRange}
 			onChange={(ev,value)=>setXRange(value)}
@@ -76,7 +77,7 @@ function f(x) {
 }
 
 function App() {
-return <div style={{margin:"0 auto"}}>
+return <div className={useStyles().root}>
 		<ChartView f={f} />
 	</div>
 }
