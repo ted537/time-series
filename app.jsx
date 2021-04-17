ba80b3a3523272ae06c2da06364538f4d0765ac1
Ted Brownlow <ted.brownlow@gmail.com>
2021-04-17 14:16:17 -0400

    fixed scaling issue

--- app.jsx
+++ app.jsx
@@ -10,6 +10,7 @@ import {
 	Charts, ChartContainer, ChartRow, 
 	YAxis, LineChart 
 } from "react-timeseries-charts";
+import { TimeSeries, TimeRange } from "pondjs";
 
 const useStyles = makeStyles({
   root: {
@@ -19,16 +20,64 @@ const useStyles = makeStyles({
   },
 });
 
-function App() {
+const X_STEP = 0.01;
+// since react-timeseries-charts scales up numbers into dates,
+// we should scale up  these values to avoid rounding errors.
+// In future, we should probably use a library 
+// that isn't focused on time-series specifically
+const TIME_SCALE = 100;
+function makeSeries(xRange,f) {
+	const [xMin,xMax] = xRange;
+	const points = [];
+	for (let x=xMin;x<xMax;x+=X_STEP) {
+		const y = f(x);
+		points.push([x*TIME_SCALE,y]);
+	}
+	return new TimeSeries({
+		columns:['time','y'],
+		points
+	});
+}
+
+function Chart({xRange,f}) {
+	const series = makeSeries(xRange,f);
+	const timeRange = new TimeRange(xRange[0]*100,xRange[1]*100);
+	return <ChartContainer timeRange={timeRange} width={800}>
+		<ChartRow height="200">
+			<YAxis 
+				id="y" label="y" 
+				min={-1} max={1} 
+				type="linear" format=",.2f"/>
+			<Charts>
+				<LineChart 
+					axis="y" series={series} columns={['y']}
+					format=",.2f"
+				/>
+			</Charts>
+		</ChartRow>
+	</ChartContainer>
+}
+
+function ChartView() {
 	const classes = useStyles();
 	const [xRange,setXRange] = useState([0,10]);
 	const [xMin,xMax] = xRange;
-	return <div class={classes.root}>
+	return <div className={classes.root}>
 		<Slider
 			value={xRange}
 			onChange={(ev,value)=>setXRange(value)}
 		/>
+		<Chart xRange={xRange} f={f} />
+	</div>
+}
 
+function f(x) {
+	return Math.sin(x);
+}
+
+function App() {
+return <div style={{margin:"0 auto"}}>
+		<ChartView f={f} />
 	</div>
 }
 
